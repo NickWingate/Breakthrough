c2e34f0d241f568a74840c284710e9b05b874084
NickWingate <nicholas.wingate03@gmail.com>
2022-03-25 14:51:04 +0000

    Partially Met

---
+++ ChallengeStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Breakthrough
+{
+    public enum ChallengeStatus
+    {
+        Unsolved,
+        Solved,
+        PartiallySolved
+    }
+}
